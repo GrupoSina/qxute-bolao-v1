2758bb0c66d9f2c93b3457d533c2060f4844fa19
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-07-09 11:12:47 -0300

    feat: menu mobile

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -13,7 +13,7 @@ import {
 import { useAuthContext } from '@/context/AuthContext'
 import { parseCookies } from 'nookies'
 import { decodeToken } from '@/utils/jwt'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 export default function App() {
   const { handleSignOut } = useAuthContext()
@@ -21,6 +21,7 @@ export default function App() {
   const decoded = decodeToken(sessionKey)
   const { push } = useRouter()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const pathname = usePathname()
 
   const menuItemsDefault = [
     {
@@ -48,6 +49,7 @@ export default function App() {
         decoded?.role === 'ADMIN' ? push('/home-admin') : push('/home-user')
         setIsMenuOpen(false)
       },
+      route: decoded?.role === 'ADMIN' ? '/home-admin' : '/home-user',
     },
     {
       menuItem: 'Partidas',
@@ -56,6 +58,7 @@ export default function App() {
         setIsMenuOpen(false)
       },
       onlyAdmin: true,
+      route: '/home-admin/matches',
     },
     {
       menuItem: 'Redefinir senha',
@@ -63,6 +66,7 @@ export default function App() {
         push('/recover-password')
         setIsMenuOpen(false)
       },
+      route: '/recover-password',
     },
 
     {
@@ -71,6 +75,7 @@ export default function App() {
         handleSignOut()
         setIsMenuOpen(false)
       },
+      route: '/',
     },
   ]
 
@@ -81,6 +86,9 @@ export default function App() {
       isBordered
       maxWidth="full"
       className="bg-[#184076] text-white"
+      classNames={{
+        menu: ['bg-black bg-opacity-60 backdrop-blur-none'],
+      }}
     >
       <NavbarContent className="">
         <NavbarMenuToggle />
@@ -94,48 +102,50 @@ export default function App() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarMenu>
-        {!sessionKey ? (
-          <>
-            {menuItemsDefault.map((item, index) => (
-              <NavbarMenuItem key={`${item}-${index}`}>
-                <Link
-                  className="w-full"
-                  color={
-                    index === 2
-                      ? 'warning'
-                      : index === menuItemsDefault.length - 1
-                        ? 'danger'
-                        : 'foreground'
-                  }
-                  href={item.route.toLowerCase()}
-                  size="lg"
-                >
-                  {item.menuItem}
-                </Link>
-              </NavbarMenuItem>
-            ))}
-          </>
-        ) : (
-          <>
-            {menuItemsAuth.map((item, index) => (
-              <div key={index}>
-                {!(item.onlyAdmin && decoded?.role !== 'ADMIN') && (
-                  <NavbarMenuItem key={`${item}-${index}`}>
-                    <Link
-                      className="w-full cursor-pointer"
-                      onPress={item.function}
-                      size="lg"
-                      color={'warning'}
-                    >
-                      {item.menuItem}
-                    </Link>
-                  </NavbarMenuItem>
-                )}
-              </div>
-            ))}
-          </>
-        )}
+      <NavbarMenu className="px-0 py-0">
+        <div className="bg-white py-4 h-full w-[50%]">
+          {!sessionKey ? (
+            <>
+              {menuItemsDefault.map((item, index) => (
+                <NavbarMenuItem key={`${item}-${index}`}>
+                  <Link
+                    className={`px-4 py-1 cursor-pointer w-full text-[#00409F] ${pathname === item.route ? 'font-bold' : ''}`}
+                    color={
+                      index === 2
+                        ? 'warning'
+                        : index === menuItemsDefault.length - 1
+                          ? 'danger'
+                          : 'foreground'
+                    }
+                    href={item.route.toLowerCase()}
+                    size="lg"
+                  >
+                    {item.menuItem}
+                  </Link>
+                </NavbarMenuItem>
+              ))}
+            </>
+          ) : (
+            <>
+              {menuItemsAuth.map((item, index) => (
+                <div key={index}>
+                  {!(item.onlyAdmin && decoded?.role !== 'ADMIN') && (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                      <Link
+                        className={`px-4 py-1 cursor-pointer w-full text-[#00409F] ${pathname === item.route ? 'font-bold' : ''}`}
+                        onPress={item.function}
+                        size="lg"
+                        color={'warning'}
+                      >
+                        {item.menuItem}
+                      </Link>
+                    </NavbarMenuItem>
+                  )}
+                </div>
+              ))}
+            </>
+          )}
+        </div>
       </NavbarMenu>
     </Navbar>
   )
